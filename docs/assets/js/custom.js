f72dc5e6947b8b41cdf187e4a977b920f087e876
iheb1221 <ihebmekni024@gmail.com>
2024-07-17 23:33:04 +0100

    init

---
+++ docs/assets/js/custom.js
@@ -0,0 +1,124 @@
+(function ($) {
+  "use strict";
+
+  function init() {
+    var nav = $('nav');
+    var navHeight = nav.outerHeight();
+
+    $('.navbar-toggler').on('click', function() {
+      if (!$('#mainNav').hasClass('navbar-reduce')) {
+        $('#mainNav').addClass('navbar-reduce');
+      }
+    });
+
+    // Preloader
+    $(window).on('load', function () {
+      if ($('#preloader').length) {
+        $('#preloader').delay(100).fadeOut('slow', function () {
+          $(this).remove();
+        });
+      }
+    });
+
+    // Back to top button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
+    });
+
+    $('.back-to-top').click(function() {
+      $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+      return false;
+    });
+
+    // ScrollTop
+    $('.scrolltop-mf').on("click", function () {
+      $('html, body').animate({ scrollTop: 0 }, 1000);
+    });
+
+    // Counter
+    $('.counter').counterUp({
+      delay: 15,
+      time: 2000
+    });
+
+    // Scrolling nav
+    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - navHeight + 5)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+
+    // Close responsive menu on scroll trigger link click
+    $('.js-scroll').on("click", function () {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: navHeight
+    });
+
+    // Navbar Menu Reduce
+    $(window).trigger('scroll');
+    $(window).on('scroll', function () {
+      var pixels = 50;
+      var top = 1200;
+      if ($(window).scrollTop() > pixels) {
+        $('.navbar-expand-md').addClass('navbar-reduce');
+        $('.navbar-expand-md').removeClass('navbar-trans');
+      } else {
+        $('.navbar-expand-md').addClass('navbar-trans');
+        $('.navbar-expand-md').removeClass('navbar-reduce');
+      }
+      if ($(window).scrollTop() > top) {
+        $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+      } else {
+        $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+      }
+    });
+    var Typed = require('typed.min.js');
+    // Function to initialize Typed.js if an element with the class .text-slider is present
+    function initializeTyped() {
+      var textSlider = document.querySelector('.text-slider');
+      if (textSlider) {
+        var typedStringsElement = document.querySelector('.text-slider-items');
+        var typedStrings = typedStringsElement.textContent;
+        new Typed(textSlider, {
+          strings: typedStrings.split(','),
+          typeSpeed: 80,
+          loop: true,
+          backDelay: 1100,
+          backSpeed: 30
+        });
+      }
+    }
+
+    // Testimonials owl
+    $('#testimonial-mf').owlCarousel({
+      margin: 20,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        }
+      }
+    });
+  }
+
+  window.initCustomJs = init;
+})(jQuery);
+// todo : fix it animation
